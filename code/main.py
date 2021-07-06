43440520077fb28c5333a3af2d1f21fc2d72c617
luismiguel.saez <luismiguel.saez@teamcmp.com>
2021-07-06 21:29:16 +0200

    Added comments

--- code/main.py
+++ code/main.py
@@ -6,12 +6,14 @@ from random import randrange
 
 def main():
 
+    # Create config object from internal mounted credentials
     try:
       config.load_incluster_config()
     except config.config_exception.ConfigException as k_exc:
       print("Kubernetes API not reachable! Exiting application ...")
       exit(1)
 
+    # Check mandatory environment variables
     for param in ["CM_NAMESPACE","CM_INTERVAL"]:
       if param not in environ:
         print("Required environment variable [%s] not set! Exiting ..." % ( param ) )
@@ -24,31 +26,38 @@ def main():
 
     v1 = client.CoreV1Api()
 
+    # Start application main loop
     while True:
       try:
+        # Get pods in the selected namespace
         pod_list = v1.list_namespaced_pod(namespace=namespace)
       except client.exceptions.ApiException as p_exec:
         print("Insuficient permissions while trying to get pods from namespace [%s]! Exiting ..." % ( namespace ) )
         exit(1)
 
+      # Iterate over items, filtering by status phase
       c = 0
+      pod_running_list = []
       for pod in pod_list.items:
-        c += 1
-        #print("%s\t%s\t%s" % (pod.metadata.name, pod.status.phase, pod.status.pod_ip))
+        if pod.status.phase == 'Running':
+          c += 1
+          pod_running_list.append(pod.metadata.name)
 
       print("Got [%s] pods from resource of type [%s]" % ( str(c), pod_list.kind ) )
 
       if c > 0:
+        # Select random index number
         pod_item_number = randrange(1,c+1)
         print("Going to kill pod number [%s] of [%s]" % ( str(pod_item_number), str(c) ) )
-        pod_item = pod_list.items[pod_item_number-1]
+        # Delete selected pod by name
         try:
-          v1.delete_namespaced_pod(pod_item.metadata.name, namespace)
+          v1.delete_namespaced_pod(pod_running_list[pod_item_number-1], namespace)
         except Exception as d_exc:
           print("Not able to delete pod due to exception: %s" % ( d_exc.message ) )
       else:
         print("Skipping, as no pods were found in namespace [%s]" % ( namespace ) )
 
+      # Sleep for the configured interval seconds
       print("Sleeping for [%s] seconds ..." % ( interval ) )
       sleep(int(interval))
 
